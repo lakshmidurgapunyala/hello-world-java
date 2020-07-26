f3861eb29a940725ca66886298c28a695fd4d45a
lakshmidurga <lakshmidurgapunyala@gmail.com>
2020-07-26 10:57:57 +0000

    modify hello devops & lakshmi

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,7 +36,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("Hello DevOps");
-    System.out.println("Hello lakshmi durga");
+    System.out.println("Hello,  DevOps");
+    System.out.println("Hello,  lakshmi durga");
   }
 }
